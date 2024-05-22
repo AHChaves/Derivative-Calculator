9602426263d2dd075cd89ca0e79070aafb3ed695
LucasR <lucas.ofc.caetano@gmail.com>
2024-05-22 13:01:34 -0300

    as

---
+++ model/modelo.py
@@ -0,0 +1,20 @@
+import string
+
+letras = string.ascii_lowercase
+coeficientes = []
+potencia = []
+count = 0
+print("coeficiente de a e b para ax^b")
+while True:
+    print(f'iteracao {count}',end="\n\n")
+    a = input("valor do coeficiente a ")
+    b = input("valor da potencia b ")
+    if a != 's' and  b != 's':
+        count +=1
+        coeficientes.append(int(a))
+        potencia.append(int(b))  
+
+        print("---------")  
+    else:
+        break
+    
