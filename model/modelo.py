20dc8fc1f1b6bc96cae4ebee0739bb3b366fbe28
LucasR <lucas.ofc.caetano@gmail.com>
2024-05-22 13:02:39 -0300

    Update modelo.py

--- model/modelo.py
+++ model/modelo.py
@@ -7,8 +7,8 @@ count = 0
 print("coeficiente de a e b para ax^b")
 while True:
     print(f'iteracao {count}',end="\n\n")
-    a = input("valor do coeficiente a ")
-    b = input("valor da potencia b ")
+    a = input("valor do coeficiente a: ")
+    b = input("valor da potencia b: ")
     if a != 's' and  b != 's':
         count +=1
         coeficientes.append(int(a))
@@ -18,3 +18,4 @@ while True:
     else:
         break
     
+
