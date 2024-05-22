cd370b67f42c16922c5131467474ee485b399856
LucasR <lucas.ofc.caetano@gmail.com>
2024-05-22 13:09:41 -0300

    as

--- model/modelo.py
+++ model/modelo.py
@@ -9,7 +9,7 @@ while True:
     print(f'iteracao {count}',end="\n\n")
     a = input("valor do coeficiente a: ")
     b = input("valor da potencia b: ")
-    if a != 's' and  b != 's':
+    if a != 's' or  b != 's':
         count +=1
         coeficientes.append(int(a))
         potencia.append(int(b))  
@@ -18,4 +18,5 @@ while True:
     else:
         break
     
-#
+for a,b in zip(coeficientes, potencia):
+    print(f'{b*a}x^{b-1}', end="")
