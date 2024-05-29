8f3a0d866ef93f438f442a2468424cd0fe703384
Arthur <85091282+AHChaves@users.noreply.github.com>
2024-05-29 13:46:08 -0300

    funções básicas

--- controler/controler.py
+++ controler/controler.py
@@ -8,14 +8,23 @@ operations = []
 def calculator():
     print("test")
 
+def clear(label):
+    global funcao
+    funcao = ""
+    label.configure(text="")
 
+def delete(label):
+    global funcao
+    tamanho = len(funcao)
+    if tamanho > 0:
+        funcao= funcao[:-1]
+        label.configure(text=funcao)
 
-
-def num(text, label):
+def num(txt, label):
     
     global funcao
     
-    funcao = funcao + str(text)
+    funcao = funcao + str(txt)
     
-    print(funcao)
-    label.set(funcao)
+    print(funcao)   
+    label.configure(text=funcao)
