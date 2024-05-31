709a58c77866221ee9fa7ecccb19b3db4a016076
Arthur <85091282+AHChaves@users.noreply.github.com>
2024-05-31 10:47:19 -0300

    agr sim funciona

--- model/calc.py
+++ model/calc.py
@@ -18,70 +18,66 @@ def separar_monomios(string_do_usuario):
     derivadas.clear()
     if string_do_usuario[0] not in '+-':
         string_do_usuario = '+' + string_do_usuario
-    mon = re.findall(r'[+-]?\d*[a-z]?\^\d+|[+-]?\d*[a-z]|[+-]?\d+', string_do_usuario)
-    # Identifica a variável na string da função
+    mon = re.findall(r'[+-]?\d*x\^\d+|[+-]?\d*x|[+-]?\d+', string_do_usuario)
     return mon
 
-# Função para separar os coeficientes, expoentes e derivadas dos monômios
 def separando_coeficiente_expoente_e_derivada(array_dos_monomios):
-    contador = 0
-    for x in array_dos_monomios:
-        pos = x.find('x')
-        if(pos != -1):
+   contador = 0
+   for x in array_dos_monomios:
+    pos = x.find("x")
+    if pos != -1:
+        try:
+            coeficientes.append(int(x[:pos]))
             try:
-                coeficientes.append(x[:pos])
-                try:
-                    expoentes.append(int(x[pos+2:]))
-                except ValueError:
-                    expoentes.append(int(1))
-                temp = coeficientes[contador] * expoentes[contador]
-                if expoentes[contador] == 1:
-                    derivadas.append(str(temp))
-                elif expoentes[contador] == 2:
-                    derivadas.append(str(temp) + 'x')
-                else:
-                    aux = expoentes[contador] - 1
-                    derivadas.append(str(temp) + 'x^' + str(aux))            
-                contador += 1
+                expoentes.append(int(x[pos+2:]))
             except ValueError:
-                if x[0] == '-':
-                    coeficientes.append(-1)
-                else:
-                    coeficientes.append(1)
-                try:
-                    expoentes.append(int(x[pos+2:]))
-                except ValueError:
-                    expoentes.append(int(1))
-                temp = coeficientes[contador] * expoentes[contador]
-                if expoentes[contador] == 1:
-                    derivadas.append(str(temp))
-                elif expoentes[contador] == 2:
-                    derivadas.append(str(temp) + 'x')
-                else:
-                    aux = expoentes[contador] - 1
-                    derivadas.append(str(temp) + 'x^' + str(aux))            
-                contador += 1
+                expoentes.append(int(1))
+            temp = coeficientes[contador] * expoentes[contador]
+            if expoentes[contador] == 1:
+                derivadas.append(str(temp))
+            elif expoentes[contador] == 2:
+                derivadas.append(str(temp)+"x")
+            else:
+                aux = expoentes[contador]-1
+                derivadas.append(str(temp)+"x^"+str(aux))            
+            contador += 1
+        except ValueError:
+            if x[0] == '-':
+                coeficientes.append(-1)
+            else:
+                coeficientes.append(1)
+            try:
+                expoentes.append(int(x[pos+2:]))
+            except ValueError:
+                expoentes.append(int(1))
+            temp = coeficientes[contador] * expoentes[contador]
+            if expoentes[contador] == 1:
+                derivadas.append(str(temp))
+            elif expoentes[contador] == 2:
+                derivadas.append(str(temp)+"x")
+            else:
+                aux = expoentes[contador]-1
+                derivadas.append(str(temp)+"x^"+str(aux))            
+            contador += 1
 
-# Função para criar a derivada da função
 def fazendoFuncaoDerivada(array_da_derivada):
     first_term = True
     funcao_derivada = ""
     for y in array_da_derivada:
         if y[0] != '-' and first_term == False:
-            funcao_derivada += "+" + y
+            funcao_derivada += "+"+y
         else:
             funcao_derivada += y
         first_term = False
     return funcao_derivada
 
-# Função para calcular a função em um determinado número
 def calcularFuncao(array, numero):
     somaFuncao = 0
     first_term = True
     for x in array:
         if array[0] == x and first_term == True and x[0] == '+':
             x = x.replace("+", "")
-        if x == f'+x' or x == f'-x':
+        if x == '+x' or x == '-x':
             x = x.replace('x', f'{numero}')
             x = x.replace('^', '**')
             somaFuncao += eval(x)
@@ -90,14 +86,13 @@ def calcularFuncao(array, numero):
             x = x.replace('^', '**')
             somaFuncao += eval(x)
         else:
-            x = x.replace('x', f'({numero})')
+            x = x.replace('x', f'*({numero})')
             x = x.replace('^', '**')
             somaFuncao += eval(x)
         first_term = False
 
     return somaFuncao
 
-# Função para calcular a reta tangente em um determinado número
 def RetaTangente(num):
     valor_funcional_funcao = calcularFuncao(monomios, num)
     valor_funcional_derivada = calcularFuncao(derivadas, num)
@@ -122,14 +117,13 @@ def RetaTangente(num):
         return "y = {0}x{1}".format(valor_funcional_derivada, b)
 
 
-# Função para calcular o valor funcional e a derivada em um determinado número
 def ValorFuncional(num):
     valor_funcional_funcao = calcularFuncao(monomios, num)
     valor_funcional_derivada = calcularFuncao(derivadas, num)
 
     return "f({0}) = {1}".format(num, valor_funcional_funcao), "f'({0}) = {1}".format(num, valor_funcional_derivada), "P({0}, {1})".format(num, valor_funcional_funcao)
 
-# Função para calcular a derivada da função
+
 def CalculaDerivada(funcao):
     monomio = separar_monomios(funcao)
     for x in monomio:
